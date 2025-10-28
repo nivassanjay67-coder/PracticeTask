59508f7cf2c939b4c3f9c2cba1086965a358bba2
nivassanjay67-coder <nivassanjay67@gmail.com>
2025-10-28 17:10:08 +0530

    Update function.js

--- Javascript/function.js
+++ Javascript/function.js
@@ -1,49 +1,49 @@
-// function samsung()
-// {
-//     console.log("This is samsung");
+function samsung()
+{
+    console.log("This is samsung");
     
-// }
-// function iphone()
-// {
-//     console.log("This is iphone");
-// }
-// function oppo()
-// {
-//     console.log("This is oppo");
+}
+function iphone()
+{
+    console.log("This is iphone");
+}
+function oppo()
+{
+    console.log("This is oppo");
     
-// }
-// function redmi()
-// {
-//     console.log("This is redmi");
+}
+function redmi()
+{
+    console.log("This is redmi");
     
-// }
-// redmi()
-// samsung ()
+}
+redmi()
+samsung ()
 
-// let a = 30
-// let b = 40
-// function add()
-// {
-//     console.log(a+b);
+let a = 30
+let b = 40
+function add()
+{
+    console.log(a+b);
     
-// }
-// add()
+}
+add()
 
-// let factor = "Vijay"
-// let fplayer = "Dhoni"
-// let fmovie = "Beast"
-// function favourit()
-// {
-//     console.log("Favourit Actor:"+factor);
-//     console.log("Favourit Player:"+fplayer);
-//     console.log("Favourit Movie:"+fmovie);
-// }
-// favourit()
+let factor = "Vijay"
+let fplayer = "Dhoni"
+let fmovie = "Beast"
+function favourit()
+{
+    console.log("Favourit Actor:"+factor);
+    console.log("Favourit Player:"+fplayer);
+    console.log("Favourit Movie:"+fmovie);
+}
+favourit()
 
-// function area(l,b)
-// {   let a = l*b
-//     console.log("Area is:"+a);
-// }
-// let length = 20
-// let breadth = 30
-// area(20,30)
+function area(l,b)
+{   let a = l*b
+    console.log("Area is:"+a);
+}
+let length = 20
+let breadth = 30
+area(20,30)
