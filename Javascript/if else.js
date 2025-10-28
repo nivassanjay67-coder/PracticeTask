74bb95a4593e862d3238c426cfa624c97925beb9
nivassanjay67-coder <nivassanjay67@gmail.com>
2025-10-28 16:32:11 +0530

    Create if else.js

---
+++ Javascript/if else.js
@@ -0,0 +1,54 @@
+// let homework = false
+// if (homework){
+//     console.log("Great Job!");
+// }
+// else{
+//     console.log("Finish your homework before playing");
+// }
+
+// let coolies_left = false
+// if(coolies_left){
+//     console.log("Would you like your cookies?");
+    
+// }
+// else{
+//     console.log("Time to bake more cookies.");
+    
+// }
+
+// let brush = false
+// if(brush){
+//     console.log("Your teeth are clean");
+    
+// }
+// else{
+//     console.log("Go brush your teeth now");
+    
+// }
+
+// let birthday = false
+// if(birthday){
+//     console.log("Happy birthday! Enjoy your specila day!");
+    
+// }else{
+//     console.log("Have a great day!");
+    
+// }
+
+// let season = "winter"
+// if(season == "spring"){
+//     console.log("Enjoy the blooming flowers.");
+    
+// }
+// if(season == "summer"){
+//     console.log("Have fun in the sun.");
+    
+// }
+// if(season == "autumn"){
+//     console.log("Admire the colourful leaves.");
+    
+// }
+// if(season =="winter"){
+//     console.log("Bundle up and stay warm");
+    
+// }
