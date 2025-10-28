f1fa7bded0ba4db1a74e3c4b5e84010b2fdad684
nivassanjay67-coder <nivassanjay67@gmail.com>
2025-10-28 14:21:58 +0530

    Create function.js

---
+++ Javascript/function.js
@@ -0,0 +1,104 @@
+// function samsung()
+// {
+//     console.log("This is samsung");
+    
+// }
+// function iphone()
+// {
+//     console.log("This is iphone");
+// }
+// function oppo()
+// {
+//     console.log("This is oppo");
+    
+// }
+// function redmi()
+// {
+//     console.log("This is redmi");
+    
+// }
+// redmi()
+// samsung ()
+
+// let a = 30
+// let b = 40
+// function add()
+// {
+//     console.log(a+b);
+    
+// }
+// add()
+
+// let factor = "Vijay"
+// let fplayer = "Dhoni"
+// let fmovie = "Beast"
+// function favourit()
+// {
+//     console.log("Favourit Actor:"+factor);
+//     console.log("Favourit Player:"+fplayer);
+//     console.log("Favourit Movie:"+fmovie);
+// }
+// favourit()
+
+// function area(l,b)
+// {   let a = l*b
+//     console.log("Area is:"+a);
+// }
+// let length = 20
+// let breadth = 30
+// area(20,30)
+
+// let homework = false
+// if (homework){
+//     console.log("Great Job!");
+// }
+// else{
+//     console.log("Finish your homework before playing");
+// }
+
+// let coolies_left = false
+// if(coolies_left){
+//     console.log("Would you like your cookies?");
+    
+// }
+// else{
+//     console.log("Time to bake more cookies.");
+    
+// }
+
+// let brush = false
+// if(brush){
+//     console.log("Your teeth are clean");
+    
+// }
+// else{
+//     console.log("Go brush your teeth now");
+    
+// }
+
+// let birthday = false
+// if(birthday){
+//     console.log("Happy birthday! Enjoy your specila day!");
+    
+// }else{
+//     console.log("Have a great day!");
+    
+// }
+
+// let season = "winter"
+// if(season == "spring"){
+//     console.log("Enjoy the blooming flowers.");
+    
+// }
+// if(season == "summer"){
+//     console.log("Have fun in the sun.");
+    
+// }
+// if(season == "autumn"){
+//     console.log("Admire the colourful leaves.");
+    
+// }
+// if(season =="winter"){
+//     console.log("Bundle up and stay warm");
+    
+// }
