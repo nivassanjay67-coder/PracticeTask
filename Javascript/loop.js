00eba156faef6dfe3a73388734df6ed20dde6624
nivassanjay67-coder <nivassanjay67@gmail.com>
2025-10-29 18:35:29 +0530

    Create loop.js

---
+++ Javascript/loop.js
@@ -0,0 +1,19 @@
+// for (i=1;i<=20;i++){
+//     console.log("Sanjay");
+// }
+
+// for(i=1;i<=10;i++){
+//     console.log(i);
+// }
+
+// let i = 1
+// while(i <= 10){
+//     console.log(i);
+//     i++
+// }
+
+// let i = 1
+// do{
+//     console.log(i);
+//     i++
+// }while(i<=10)
