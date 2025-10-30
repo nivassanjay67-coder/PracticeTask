6c4facff9cb543ff623dce466fa4e7bdf770bde0
nivassanjay67-coder <nivassanjay67@gmail.com>
2025-10-30 20:12:21 +0530

    Update function.js

--- Javascript/function.js
+++ Javascript/function.js
@@ -1,49 +1,72 @@
-function samsung()
-{
-    console.log("This is samsung");
+// function samsung()
+// {
+//     console.log("This is samsung");
     
-}
-function iphone()
-{
-    console.log("This is iphone");
-}
-function oppo()
-{
-    console.log("This is oppo");
+// }
+// function iphone()
+// {
+//     console.log("This is iphone");
+// }
+// function oppo()
+// {
+//     console.log("This is oppo");
     
-}
-function redmi()
-{
-    console.log("This is redmi");
+// }
+// function redmi()
+// {
+//     console.log("This is redmi");
     
-}
-redmi()
-samsung ()
+// }
+// redmi()
+// samsung ()
 
-let a = 30
-let b = 40
-function add()
-{
-    console.log(a+b);
+// let a = 30
+// let b = 40
+// function add()
+// {
+//     console.log(a+b);
     
-}
-add()
+// }
+// add()
 
-let factor = "Vijay"
-let fplayer = "Dhoni"
-let fmovie = "Beast"
-function favourit()
-{
-    console.log("Favourit Actor:"+factor);
-    console.log("Favourit Player:"+fplayer);
-    console.log("Favourit Movie:"+fmovie);
-}
-favourit()
+// let factor = "Vijay"
+// let fplayer = "Dhoni"
+// let fmovie = "Beast"
+// function favourit()
+// {
+//     console.log("Favourit Actor:"+factor);
+//     console.log("Favourit Player:"+fplayer);
+//     console.log("Favourit Movie:"+fmovie);
+// }
+// favourit()
 
-function area(l,b)
-{   let a = l*b
-    console.log("Area is:"+a);
-}
-let length = 20
-let breadth = 30
-area(20,30)
+// function area(l,b)
+// {   let a = l*b
+//     console.log("Area is:"+a);
+// }
+// let length = 20
+// let breadth = 30
+// area(20,30)
+
+// functions with parameter
+// function login(username,password){
+//     console.log("This is",username,password);
+// }
+// login("Sanjay","Divya")
+
+// function with return value
+// function add(a,b){
+//     return a+b;
+// }
+// console.log(add(300,700));
+
+// arrow functions
+// const user = () =>{
+//     console.log("Sanjay");
+// }
+// user()
+
+// const login = (username,password) =>{
+//     console.log("This is",username,password);
+// }
+// login("Sanjay's", "Divya")
